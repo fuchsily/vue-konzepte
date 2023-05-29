3b89e3285a52f622aef682d69783967dfe1abcb2
fuchsily <victoria.fuchs@arcor.de>
2023-05-29 15:10:02 +0200

    added counter component

--- src/components/views/AboutPage.vue
+++ src/components/views/AboutPage.vue
@@ -1,7 +1,23 @@
+<script>
+import ButtonCounter from "@/components/counter/CounterComponent.vue";
+
+export default {
+  components: {
+    ButtonCounter
+  }
+}
+</script>
+
+
+
 <template>
     <div>
       <h2>Über uns</h2>
       <p>Dies ist eine Beispielanwendung, die Vue.js verwendet.</p>
+    </div>
+    <div>
+      <h1>Here is a child component!</h1>
+      <ButtonCounter />
     </div>
 </template>
   
